e05ba5e1a10f7fcaa63b37b7ffbc02be20fa5e2b
PashkovGrigoriy <gripas2006@gmail.com>
2025-09-17 01:10:37 +0500

    Task2

---
+++ Tyuiu.PashkovGV.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.PashkovGV.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.PashkovGV.Sprint0.Task2.V0
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Игорь"));
+            Console.ReadKey();
+        }
+    }
+}
