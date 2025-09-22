d891db06b4aa266fc2e1c87becef1bfa40d87dc6
PashkovGrigoriy <gripas2006@gmail.com>
2025-09-22 12:59:27 +0500

    Task6

---
+++ Tyuiu.PashkovGV.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.PashkovGV.Sprint0.Task6.V0.Lib;
+namespace Tyuiu.PashkovGV.Sprint0.Task6.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arraynums = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Сумма элеиентов массива = " + DataService.AdditionArray(arraynums));
+            Console.WriteLine("Разность элементов массива = " + DataService.SubstractionArray(arraynums));
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(arraynums));
+            Console.ReadKey();
+        }
+    }
+}
